10fd2230441b1af81235faaa4174b07da3381451
Artur Costa <souza.artur@edu.univali.br>
2023-08-19 00:58:07 -0300

    carrossel com splide

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const splide = new Splide( '.splide', {
+    grid: {
+        rows: 2,
+        cols: 3,
+        gap : {
+            row: '2%',
+            col: '2%',
+        },
+    },
+} ).mount( window.splide.Extensions );
+
+
+
